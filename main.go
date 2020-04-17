bf143dc2fbabdc9eb73276b7acf14e4a860eec30
maxsynnott <maxryansynnott@gmail.com>
2020-04-17 12:36:58 +1200

    added seconds flag

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ import (
 )
 
 func main() {
-	// Open and read the csv file
+	// Parse flags
 	fileName := flag.String("csv", "./questions.csv", "path to questions csv")
+	seconds := flag.Int("seconds", 30, "timer length in seconds")
 
 	flag.Parse()
+	// 
 
+	// Open and read the csv file
 	questionsFile, err := os.Open(*fileName)
 
 	if err != nil {
@@ -32,12 +35,12 @@ func main() {
 	// 
 
 	var score int
-	fmt.Println("Please answer the following questions:")
+
+	fmt.Println("Press enter to start the quiz")
+	consoleReader.ReadString('\n')
 
 	// Initiate Timer
-	const seconds = 20
-
-	time.AfterFunc(time.Second * seconds, func() {
+	time.AfterFunc(time.Second * time.Duration(*seconds), func() {
 		fmt.Println("Times up!")
 	  fmt.Printf("You got %d out of %d questions correct.", score, len(questions))
 		os.Exit(1)
@@ -45,6 +48,7 @@ func main() {
 	// 
 
 	// Iterate through each question asking questions and updating score
+	fmt.Println("Please answer the following questions:")
 	for i := range questions {
 		question := questions[i][0]
 		answer := questions[i][1]
