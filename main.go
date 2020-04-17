4cc5e1e01b3bbc3ebcda377a73a67a610fecc2eb
maxsynnott <maxryansynnott@gmail.com>
2020-04-17 12:01:52 +1200

    basic quiz up and running

--- main.go
+++ main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"encoding/csv"
+	"bufio"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// Open and read the csv file
+	questionsFile, err := os.Open("./questions.csv")
+	if err != nil {
+		fmt.Println("An error encountered ::", err)
+	}
+
+	csvReader := csv.NewReader(questionsFile)
+	questions, err := csvReader.ReadAll()
+	// 
+
+	// Initiialize the console input reader
+	consoleReader := bufio.NewReader(os.Stdin)
+	// 
+
+	// Iterate through each question
+	fmt.Println("Please answer the following questions:")
+
+	var score int
+
+	for i := range questions {
+		question := questions[i][0]
+		answer := questions[i][1]
+
+		fmt.Printf("Question #%d: %s\n", i, question)
+
+		text, _ := consoleReader.ReadString('\n')
+		userAnswer := strings.TrimSpace(text)
+
+		if userAnswer == answer {
+			score += 1
+		}
+	}
+	//
+
+	fmt.Printf("You got %d out of %d questions correct.", score, len(questions))
 }
